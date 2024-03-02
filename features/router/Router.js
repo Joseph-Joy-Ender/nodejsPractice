10885c6d339ff229a9ef33fced7c12280c53820b
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-03-02 15:14:00 +0100

    "node practical changes"

--- features/router/Router.js
+++ features/router/Router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {register, login, deleteUser} = require('../controller/userController');
-// const {login} = require("../service/userService");
+
 
 router.route('/register').post(register);
 router.route('/login').post(login)
