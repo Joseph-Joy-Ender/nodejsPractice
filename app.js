387b10401645c959f010a52658034ff4e166ca29
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-03-05 12:15:21 +0100

    installing cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const dbUrl = process.env.MONGO_URI
 const Connect = require('./features/db/connectDB');
 const router = require('./features/router/Router')
@@ -9,6 +10,7 @@ const notFound = require('./features/middleware/NotFound');
 
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/user', router)
 app.use(notFound);
 
